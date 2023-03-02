f999221962c65e64a01c3ce46aa0dae70abe02c4
jmartins9 <joaoantonioredondo1994@gmail.com>
2023-03-02 20:28:18 +0000

    corrigi er

--- TPC3/exc.py
+++ TPC3/exc.py
@@ -5,14 +5,13 @@ def main():
     with open("processos.txt", "r") as file:
         lines = file.readlines()
 
-    er_relationships = re.compile(r",([a-zA-Z][a-zA-Z ]*?)\.( P|:)")
+    er_relationships = re.compile(r"[a-z],([A-Z][a-zA-Z ]*?)\.")
     relationships = {}
 
     for line in lines:
         relationships_list = er_relationships.findall(line)
 
         for relationship in relationships_list:
-            relationship = relationship[0]
             if relationship not in relationships:
                 relationships[relationship] = 0
             relationships[relationship] += 1
