1306f9f3e3923c6a2dad5b55a342a77fb7d58821
jmartins9 <joaoantonioredondo1994@gmail.com>
2023-03-11 18:08:30 +0000

    TCP4

--- TPC4/csvToJson.py
+++ TPC4/csvToJson.py
@@ -3,46 +3,45 @@ import json
 
 
 def main():
-    header_re = re.compile(r"([\wú]+)({\d+,\d+}|{\d+})?(::\w+)?")
+    header_re = re.compile(r"(\w+)(?:{(\d+)(?:,(\d+))})?(?:::(\w+))?")
     body_re = re.compile(r"\s*,\s*")
-    intf_re = re.compile(r"{(\d+),(\d+)}")
-    nf_re = re.compile(r"{(\d+)}")
 
     with open("students.csv", "r", encoding="UTF-8") as file:
         header = file.readline()
         lines = list(map(lambda x: body_re.split(x[:-1]), file.readlines()))
+        lines.pop()
 
-    lines.pop()
     headers = header_re.findall(header)
     students = []
+
     for line in lines:
         student = {}
 
         for index, head in enumerate(headers):
-            if head[1] == '' and head[2] == '':
+
+            if head[1] == '' and head[2] == '' and head[3] == '':
                 student[head[0]] = line[index]
             else:
-                result = nf_re.match(head[1])
-                result1 = intf_re.match(head[1])
                 grades = []
-                if result:
-                    for i in range(int(result.group(1))):
-                        grades.append(line[index + i])
-                elif result1:
-                    for i in range(int(result1.group(2))):
-                        if line[index + i] != '':
-                            grades.append(line[index + i])
-
                 if head[2] != '':
-                    func = (head[2])[2:]
-                else:
-                    func = ''
-                    student[head[0]] = grades
+                    for i in range(int(head[2])):
+                        if line[index + i] != '':
+                            grades.append(int(line[index + i]))
+                elif head[1] != '':
+                    for i in range(int(head[1])):
+                        grades.append(int(line[index + i]))
 
-                if func == 'sum' and len(grades) != 0:
-                    student["Notas_sum"] = sum(map(lambda x: int(x), grades))
-                elif func == 'media' and len(grades) != 0:
-                    student["Notas_media"] = sum(map(lambda x: int(x), grades)) / len(grades)
+                match head[3]:
+                    case 'sum':
+                        student["Notas_sum"] = sum(grades)
+                    case 'media':
+                        student["Notas_media"] = sum(grades) / len(grades)
+                    case 'max':
+                        student["Nota_maxima"] = max(grades)
+                    case 'min':
+                        student["Notas_minima"] = min(grades)
+                    case '':
+                        student[head[0]] = grades
 
         students.append(student)
 
